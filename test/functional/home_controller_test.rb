1974531905dc8a424eb071d9ed82d033c3d9dd49
Kyle Maxwell <kyle@111.flood.pivotallabs.com>
2009-02-11 11:33:17 -0800

    refactored home controller test

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,21 +1,23 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  TYPES = :domains, :users, :sprigs, :parselets
 
   def setup
-    HomeController.send :attr_accessor, :domains, :users, :sprigs, :parselets
+    HomeController.send :attr_accessor, *TYPES
   end
 
   def test_routing
     assert_routing "/", :controller => "home", :action => "index"
-    
   end
   
   def test_content
     get :index
-    assert_not_nil @controller.domains
-    assert_not_nil @controller.users
-    assert_not_nil @controller.sprigs
-    assert_not_nil @controller.parselets
+    TYPES.each do |method|
+      assert_not_nil @controller.send(method)
+    end
+    
+    assert_select "h3", TYPES.length
+    assert_select "ol", TYPES.length
   end
 end
