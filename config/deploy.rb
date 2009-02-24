d9286880f572e371b964b2d793b56f0e792f8102
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2009-02-24 12:07:52 -0800

    configuration

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "parselets"
+set :repository,  "git@github.com:fizx/parselets_org.git"
+set :deploy_to, "/var/apps/#{application}"
+set :scm, :git
+set :branch, "master"
+set :user, "www"
+set :use_sudo, false
+
+SERVER = "parselets.org"
+
+role :app, SERVER
+role :web, SERVER
+role :db,  SERVER, :primary => true
+
+namespace :deploy do
+  task :before_restart do
+    run "cd #{current_path} && rake db:migrate --trace RAILS_ENV=production"
+    run "cd #{current_path} && rake us:boot --trace RAILS_ENV=production"
+  end
+  
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  task :after_restart do
+    system %[echo "deploy complete" | growlnotify "parselets.com"]
+  end
+end
