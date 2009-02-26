45f3d75e17c6df401f4051ca8fd41a0d36444c3d
Kyle Maxwell <kyle@111.flood.pivotallabs.com>
2009-02-26 11:06:27 -0800

    password

--- app/models/password_request.rb
+++ app/models/password_request.rb
@@ -1,7 +1,6 @@
 class PasswordRequest < ActiveRecord::Base
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   validates_each :email do |rec, attr, val|
