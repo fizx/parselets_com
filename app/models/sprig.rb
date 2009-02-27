f14bc9fb9fb06dbdb38ddd9be77a4fbfea2d7e87
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2009-02-26 20:07:32 -0800

    progress

--- app/models/sprig.rb
+++ app/models/sprig.rb
@@ -17,8 +17,16 @@ class Sprig < ActiveRecord::Base
     ],
     :conditions => "sprigs.deleted_at IS NULL AND user_id IS NOT NULL",
     :order => "sprigs.updated_at DESC", :delta => true
-
+    
+  validates_presence_of :name, :description, :code
+  validates_format_of :name, :with => /\A[a-z0-9\-_]*\Z/, :message => "contains invalid characters"
+  
+  def keys=(a)
+    self.code = a.inspect
+  end
+  
+  
   def keys
-    []
+    eval(code || "[]").map{|a| a unless a.blank? }.compact
   end
 end
