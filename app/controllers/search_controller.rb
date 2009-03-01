bd341358092b723b097b9a4a6090cbd0e61a5656
Andrew Cantino <cantino@gmail.com>
2009-03-01 11:42:50 -0800

    backend for gem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,27 @@ class SearchController < ApplicationController
   around_filter :dynamic_scope
   
   def index
-    class_names = (params[:classes] || "Parselet,Sprig,User,Domain").split(",")
+    respond_to do |wants|
+      wants.html { do_search }
+      wants.json do
+        do_search "Parselet"
+        render :json => @search.results.to_json
+      end
+    end
+  end
+
+protected
+
+  def preprocess(q)
+    q.gsub(%r[http://]i, '')
+  end
+
+  def do_search(class_names = nil)
+    class_names = class_names || (params[:classes] || "Parselet,Sprig,User,Domain").split(",")
     @search = Ultrasphinx::Search.new(:query => preprocess(params[:q]), 
       :class_names => class_names, 
       :page => params[:page] || 1,
       :per_page => 10)
     @search.run
-  end
-protected
-  def preprocess(q)
-    q.gsub(%r[http://]i, '')
   end
 end
