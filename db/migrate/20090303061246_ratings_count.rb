dde2be64a93d85944826bf5e10e24f4ab6b8d283
Kyle Maxwell <kyle@111.flood.pivotallabs.com>
2009-03-03 14:04:32 -0800

    make mysql happy

--- db/migrate/20090303061246_ratings_count.rb
+++ db/migrate/20090303061246_ratings_count.rb
@@ -1,9 +1,9 @@
 class RatingsCount < ActiveRecord::Migration
   def self.up
-    add_column :parselets, :ratings_count, :integer, :null => false
-    add_column :parselet_versions, :ratings_count, :integer, :null => false
-    add_column :parselets, :comments_count, :integer, :null => false
-    add_column :parselet_versions, :comments_count, :integer, :null => false
+    add_column :parselets, :ratings_count, :integer, :null => false, :default => 0
+    add_column :parselet_versions, :ratings_count, :integer, :null => false, :default => 0
+    add_column :parselets, :comments_count, :integer, :null => false, :default => 0
+    add_column :parselet_versions, :comments_count, :integer, :null => false, :default => 0
   end
 
   def self.down
--- db/migrate/20090303061503_cached_rating.rb
+++ db/migrate/20090303061503_cached_rating.rb
@@ -1,7 +1,7 @@
 class CachedRating < ActiveRecord::Migration
   def self.up
-    add_column :parselets, :cached_rating, :integer, :null => false
-    add_column :parselet_versions, :cached_rating, :integer, :null => false
+    add_column :parselets, :cached_rating, :integer, :null => false, :default => 0
+    add_column :parselet_versions, :cached_rating, :integer, :null => false, :default => 0
   end
 
   def self.down
