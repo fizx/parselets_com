6c10da505fd738b51d8e13180752d27019b453b2
Andrew Cantino <cantino@gmail.com>
2009-03-02 16:16:45 -0800

    json results ignore broken parselets

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,27 @@ class SearchController < ApplicationController
     respond_to do |wants|
       wants.html { do_search }
       wants.json do
-        do_search "Parselet"
-        render :json => @search.results.to_json
+        do_search :class_names => "Parselet"
+        render :json => remove_broken(@search.results).to_json
       end
     end
   end
 
 protected
 
+  def remove_broken(parselets)
+    parselets.inject([]) do |memo, parselet|
+      memo << parselet if parselet.try(:works)
+      memo
+    end
+  end
+
   def preprocess(q)
     q.gsub(%r[http://]i, '')
   end
 
-  def do_search(class_names = nil)
-    class_names = class_names || (params[:classes] || "Parselet,Sprig,User,Domain").split(",")
+  def do_search(options = {})
+    class_names = options[:class_names] || (params[:classes] || "Parselet,Sprig,User,Domain").split(",")
     @search = Ultrasphinx::Search.new(:query => preprocess(params[:q]), 
       :class_names => class_names, 
       :page => params[:page] || 1,
