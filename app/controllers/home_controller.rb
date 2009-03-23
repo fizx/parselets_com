949eb0519d0be686771b5befb25af46416dff923
Andrew Cantino <cantino@gmail.com>
2009-03-22 22:42:06 -0700

    favorites optimization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   layout "simple"
   
   def index
-    @parselets = Parselet.advanced_find :all, :limit => 5
+    @parselets = Parselet.advanced_find :all, :limit => 5, :favorite_user => current_user
   end
 end
