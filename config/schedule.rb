dbd073244a6a4b852fb53d4cc3dda1af5b1ae8ba
Kyle Maxwell <kyle@111.flood.pivotallabs.com>
2009-03-25 14:21:53 -0700

    add cron log

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,21 +3,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :cron_log, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+set :cron_log, "/var/www/parselets/shared/log/whenever.log"
 
 every 1.day, :at => "12 am" do
   rake "parselet:check"
