1c6a7d5836071ae5e068ca6d39a721023e3e9291
Andrew Cantino <cantino@gmail.com>
2009-03-30 20:59:12 -0700

    missed one

--- db/migrate/20090329065321_add_allow_user_contact_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAllowUserContactToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :allow_user_contact, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :users, :allow_user_contact
-  end
-end
